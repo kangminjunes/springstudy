8b58cd826a8c6a5cedc3d3657b2f8a50f36fa563
kangminjunes <1999koco@naver.com>
2023-09-27 16:39:08 +0900

    20230927

--- 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController02.java
+++ 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController02.java
@@ -11,16 +11,17 @@ public class MyController02 {
   // 2. @PostMapping
   
   @GetMapping(value="/notice/list.do")
-  public String boardList() {
+  public String noticeList() {   // 해당 메소드가 가지는 이름의의미는없다
     // ViewResolver의 prefix : /WEB-INF/views/
     // ViewResolver의 suffix : .jsp
-    return "notice/list";  //   boardList/notice/list.jsp;
+    return "notice/list";  //  /WEB-INF/views/notice/list.jsp;
   }
   
   // 반환이 없는 경우에는 주소 요청 주소를 Jsp 경로로 인식한다.
   // /member/list.do 요청을 /member/list.jsp 경로로 인식한다.
   // /member/list    요청을 /member/list.jsp 경로로 인식한다.
-  @GetMapping(value="/member/list.do")
+  // 여러개의 요청을 하고싶다면 {} 중괄호를 활용하여 배열로 만드면된다.
+  @GetMapping(value={"/member/list.do","/member/list"})
   public void memberList() {
     
   }
