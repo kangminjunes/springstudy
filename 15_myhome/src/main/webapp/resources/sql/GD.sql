5180582cca18a70fcec67995bd373da45ff519c2
kangminjunes <1999koco@naver.com>
2023-10-25 17:10:01 +0900

    20231025

--- 15_myhome/src/main/webapp/resources/sql/GD.sql
+++ 15_myhome/src/main/webapp/resources/sql/GD.sql
@@ -1,10 +1,18 @@
 -- 시퀀스
 DROP SEQUENCE USER_SEQ;
+DROP SEQUENCE FREE_SEQ;
+DROP SEQUENCE BLOG_SEQ;
+DROP SEQUENCE COMMENT_SEQ;
 
 CREATE SEQUENCE USER_SEQ NOCACHE;
-
+CREATE SEQUENCE FREE_SEQ NOCACHE;
+CREATE SEQUENCE BLOG_SEQ NOCACHE;
+CREATE SEQUENCE COMMENT_SEQ NOCACHE;
 
 -- 테이블
+DROP TABLE COMMENT_T;
+DROP TABLE BLOG_T;
+DROP TABLE FREE_T;
 DROP TABLE INACTIVE_USER_T;
 DROP TABLE LEAVE_USER_T;
 DROP TABLE ACCESS_T;
@@ -14,7 +22,7 @@ DROP TABLE USER_T;
 CREATE TABLE USER_T (
     USER_NO        NUMBER              NOT NULL,        -- PK
     EMAIL          VARCHAR2(100 BYTE)  NOT NULL UNIQUE, -- 이메일을 아이디로 사용
-    PW             VARCHAR2(64 BYTE)   NOT NULL,        -- SHA-256 암호화 방식 사용
+    PW             VARCHAR2(64 BYTE),                   -- SHA-256 암호화 방식 사용
     NAME           VARCHAR2(50 BYTE),                   -- 이름
     GENDER         VARCHAR2(2 BYTE),                    -- M, F, NO
     MOBILE         VARCHAR2(15 BYTE),                   -- 하이픈 제거 후 저장
@@ -23,6 +31,7 @@ CREATE TABLE USER_T (
     JIBUN_ADDRESS  VARCHAR2(100 BYTE),                  -- 지번주소
     DETAIL_ADDRESS VARCHAR2(100 BYTE),                  -- 상세주소
     AGREE          NUMBER              NOT NULL,        -- 서비스 동의 여부(0:필수, 1:이벤트)
+    STATE          NUMBER,                              -- 가입형태(0:정상 1:네이버)
     PW_MODIFIED_AT DATE,                                -- 비밀번호 수정일
     JOINED_AT      DATE,                                -- 가입일
     CONSTRAINT PK_USER PRIMARY KEY(USER_NO)
@@ -46,7 +55,7 @@ CREATE TABLE LEAVE_USER_T (
 CREATE TABLE INACTIVE_USER_T (
     USER_NO        NUMBER              NOT NULL,        -- PK
     EMAIL          VARCHAR2(100 BYTE)  NOT NULL UNIQUE, -- 이메일을 아이디로 사용
-    PW             VARCHAR2(64 BYTE)   NOT NULL,        -- SHA-256 암호화 방식 사용
+    PW             VARCHAR2(64 BYTE),                   -- SHA-256 암호화 방식 사용
     NAME           VARCHAR2(50 BYTE),                   -- 이름
     GENDER         VARCHAR2(2 BYTE),                    -- M, F, NO
     MOBILE         VARCHAR2(15 BYTE),                   -- 하이픈 제거 후 저장
@@ -55,6 +64,7 @@ CREATE TABLE INACTIVE_USER_T (
     JIBUN_ADDRESS  VARCHAR2(100 BYTE),                  -- 지번주소
     DETAIL_ADDRESS VARCHAR2(100 BYTE),                  -- 상세주소
     AGREE          NUMBER              NOT NULL,        -- 서비스 동의 여부(0:필수, 1:이벤트)
+    STATE          NUMBER,                              -- 가입형태(0:정상 1:네이버)
     PW_MODIFIED_AT DATE,                                -- 비밀번호 수정일
     JOINED_AT      DATE,                                -- 가입일
     INACTIVED_AT   DATE,                                -- 휴면처리일
@@ -62,16 +72,78 @@ CREATE TABLE INACTIVE_USER_T (
 );
 
 
+-- 자유게시판(계층형-1차, 댓글/대댓글 작성 가능)
+CREATE TABLE FREE_T (
+    FREE_NO     NUMBER             NOT NULL,
+    EMAIL       VARCHAR2(100 BYTE)  NULL,
+    CONTENTS    VARCHAR2(4000 BYTE) NOT NULL,
+    CREATED_AT  TIMESTAMP           NULL,
+    STATUS      NUMBER              NOT NULL,   -- 1:정상 게시글, 0:삭제된 게시글(실제로 삭제되지 않는 게시판)
+    DEPTH       NUMBER              NOT NULL,   -- 0:원글 1:댓글 2:대댓글 .... N:대대대대대대...댓글    
+    GROUP_NO    NUMBER              NOT NULL,   -- 원글과 모든 댓글(댓글,대댓글)은 동일한 GROPU_NO를 가져야 함.
+    GROUP_ORDER NUMBER              NOT NULL,   -- 같은 그룹 내 정렬 순서
+    CONSTRAINT PK_FREE PRIMARY KEY(FREE_NO),
+    CONSTRAINT FK_USER_FREE FOREIGN KEY(EMAIL) REFERENCES USER_T(EMAIL) ON DELETE SET NULL
+);
+
+-- 블로그(댓글형)
+CREATE TABLE BLOG_T (
+    BLOG_NO     NUMBER             NOT NULL,    -- 블로그 번호
+    TITLE       VARCHAR2(500 BYTE) NOT NULL,    -- 제목
+    CONTENTS    CLOB               NULL,        -- 내용
+    USER_NO     NUMBER             NOT NULL,    -- 작성자 번호(NULL인 경우 ON DELETE SET NULL 처리가 가능하고, NOT NULL인 경우 ON DELETE CASCADE 처리가 가능하다.)
+    HIT         NUMBER             DEFAULT 0,   -- 조회수
+    IP          VARCHAR2(30 BYTE)  NULL,        -- IP 주소
+    CREATED_AT  VARCHAR2(30 BYTE)  NULL,        -- 작성일
+    MODIFIED_AT VARCHAR2(30 BYTE)  NULL,        -- 수정일
+    CONSTRAINT PK_BLOG PRIMARY KEY(BLOG_NO),
+    CONSTRAINT FK_USER_BLOG FOREIGN KEY(USER_NO)REFERENCES USER_T(USER_NO) ON DELETE CASCADE -- 작성자가 삭제되면 블로그도 함께 삭제된다.
+);
+
+-- 블로그 댓글(계층형)
+CREATE TABLE COMMENT_T (
+    COMMENT_NO NUMBER NOT NULL,
+    CONTENTS   VARCHAR2(4000 BYTE) NULL,
+    USER_NO    NUMBER              NULL,
+    BLOG_NO    NUMBER              NOT NULL,
+    CREATED_AT VARCHAR2(30 BYTE)   NULL,
+    CONSTRAINT PK_COMMENT PRIMARY KEY(COMMENT_NO),
+    CONSTRAINT FK_USER_COMMENT FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO) ON DELETE SET NULL,
+    CONSTRAINT FK_BLOG_COMMENT FOREIGN KEY(BLOG_NO) REFERENCES BLOG_T(BLOG_NO) ON DELETE CASCADE 
+);
+
+
 -- 테스트용 INSERT
-INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user1@naver.com', STANDARD_HASH('1111', 'SHA256'), '사용자1', 'M', '01011111111', '11111', '디지털로', '가산동', '101동 101호', 0, TO_DATE('20231001', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
-INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user2@naver.com', STANDARD_HASH('2222', 'SHA256'), '사용자2', 'F', '01022222222', '22222', '디지털로', '가산동', '101동 101호', 0, TO_DATE('20230801', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
-INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user3@naver.com', STANDARD_HASH('3333', 'SHA256'), '사용자3', 'NO', '01033333333', '33333', '디지털로', '가산동', '101동 101호', 0, TO_DATE('20230601', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
+INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user1@naver.com', STANDARD_HASH('1111', 'SHA256'), '사용자1', 'M', '01011111111', '11111', '디지털로', '가산동', '101동 101호', 0, 0, TO_DATE('20231001', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
+INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user2@naver.com', STANDARD_HASH('2222', 'SHA256'), '사용자2', 'F', '01022222222', '22222', '디지털로', '가산동', '101동 101호', 0, 0, TO_DATE('20230801', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
+INSERT INTO USER_T VALUES(USER_SEQ.NEXTVAL, 'user3@naver.com', STANDARD_HASH('3333', 'SHA256'), '사용자3', 'NO', '01033333333', '33333', '디지털로', '가산동', '101동 101호', 0, 0, TO_DATE('20230601', 'YYYYMMDD'), TO_DATE('20220101', 'YYYYMMDD'));
 
 INSERT INTO ACCESS_T VALUES('user1@naver.com', TO_DATE('20231018', 'YYYYMMDD'));  -- 정상 회원 (user1)
 INSERT INTO ACCESS_T VALUES('user2@naver.com', TO_DATE('20220201', 'YYYYMMDD'));  -- 휴면 회원 (user2)
                                                                                   -- 휴면 회원 (user3)
 COMMIT;
 
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+INSERT INTO FREE_T VALUES (FREE_SEQ.NEXTVAL, 'user2@naver.com', '내용1', SYSTIMESTAMP, 1, 0, FREE_sEQU.CURRVAL, 0);
+COMMIT;
 
 -- 쿼리 테스트
 
