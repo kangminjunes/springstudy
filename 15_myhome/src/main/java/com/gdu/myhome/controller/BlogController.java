d357210f212ab265a6a1e1e612b008efd3a97f50
kangminjunes <1999koco@naver.com>
2023-10-31 16:26:53 +0900

    20231031

--- 15_myhome/src/main/java/com/gdu/myhome/controller/BlogController.java
+++ 15_myhome/src/main/java/com/gdu/myhome/controller/BlogController.java
@@ -68,6 +68,29 @@ public class BlogController {
     return "blog/detail";
   }
   
+  @PostMapping("/edit.form")
+  public String edit(@RequestParam(value="blogNo", required=false, defaultValue="0") int blogNo
+                   , Model model) {
+    BlogDto blog = blogService.getBlog(blogNo);
+    model.addAttribute("blog", blog);
+    return "blog/edit";    
+  }
+  
+  @PostMapping("/modifyBlog.do")
+  public String modifyBlog(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    int modifyResult = blogService.modifyBlog(request);
+    redirectAttributes.addFlashAttribute("modifyResult", modifyResult);
+    return "redirect:/blog/detail.do?blogNo=" + request.getParameter("blogNo");
+  }
+  
+  @PostMapping("/remove.do")
+  public String remove(@RequestParam(value="blogNo", required=false, defaultValue="0") int blogNo
+                      , RedirectAttributes attr) {
+    int removeResult = blogService.removeBlog(blogNo);
+    attr.addFlashAttribute("removeResult", removeResult);
+    return "redirect:/blog/list.do";
+  }
+  
   @ResponseBody
   @PostMapping(value="/addComment.do", produces="application/json")
   public Map<String, Object> addComment(HttpServletRequest request) {
@@ -80,7 +103,17 @@ public class BlogController {
     return blogService.loadCommentList(request);
   }
   
+  @ResponseBody
+  @PostMapping(value="/addCommentReply.do", produces="application/json")
+  public Map<String, Object> addCommentReply(HttpServletRequest request) {
+    return blogService.addCommentReply(request);
+  }  
   
+  @ResponseBody
+  @PostMapping(value="/removeComment.do",  produces="application/json")
+  public Map<String, Object> removeComment(@RequestParam(value="commentNo", required=false, defaultValue ="0") int commentNo){
+   return blogService.removeComment(commentNo);
+  }
   
   
 }
